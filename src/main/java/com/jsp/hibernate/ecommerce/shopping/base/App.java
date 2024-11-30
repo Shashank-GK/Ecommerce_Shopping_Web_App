292b7c54b26568645020a8cd6047a05d82877c43
Shashank GK <89540267+Shashank-GK@users.noreply.github.com>
2024-11-30 18:19:09 +0530

    First Commit

---
+++ src/main/java/com/jsp/hibernate/ecommerce/shopping/base/App.java
@@ -0,0 +1,13 @@
+package com.jsp.hibernate.ecommerce.shopping.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
